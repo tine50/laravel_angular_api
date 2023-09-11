39682a9736eb424791e583b3ce989bf602319a0b
Abdoussalam_TINE <abdoussalamtine4@gmail.com>
2023-09-11 01:01:15 +0000

    First laravel api 👌👌👌

---
+++ app/Http/Controllers/ClasseController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Classe;
+use App\Http\Requests\StoreClasseRequest;
+use App\Http\Requests\UpdateClasseRequest;
+
+class ClasseController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreClasseRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreClasseRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Classe  $classe
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Classe $classe)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Classe  $classe
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Classe $classe)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateClasseRequest  $request
+     * @param  \App\Models\Classe  $classe
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateClasseRequest $request, Classe $classe)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Classe  $classe
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Classe $classe)
+    {
+        //
+    }
+}
